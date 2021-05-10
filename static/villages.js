60cc80c3624021a92f095b9ac5cced777a30c91d
Charles <charleslanop@gmail.com>
2021-05-09 22:13:40 -0300

    updated

---
+++ static/villages.js
@@ -0,0 +1,54 @@
+const villages = [
+    {
+        name: 'VILLAGE_A',
+        image: 1,
+        value: 1
+    },
+    {
+        name: 'VILLAGE_B',
+        image: 2,
+        value: 2
+    },
+    {
+        name: 'VILLAGE_C',
+        image: 3,
+        value: 3
+    },
+    {
+        name: 'VILLAGE_D',
+        image: 4,
+        value: 4
+    },
+    {
+        name: 'VILLAGE_E',
+        image: 5,
+        value: 5
+    },
+    {
+        name: 'VILLAGE_F',
+        image: 6,
+        value: 6
+    },
+    {
+        name: 'VILLAGE_G',
+        image: 7,
+        value: 7
+    },
+    {
+        name: 'VILLAGE_H',
+        image: 8,
+        value: 8
+    },
+    {
+        name: 'VILLAGE_I',
+        image: 9,
+        value: 9
+    },
+    {
+        name: 'VILLAGE_J',
+        image: 10,
+        value: 10
+    }
+]
+
+export default villages;
